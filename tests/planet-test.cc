81ebd787940685e07debd7fc89b8809ee0dc4d5b
venyajoshi <venyaj2@illinois.edu>
2021-04-22 00:37:29 -0500

    Tested to see if planets went through

--- tests/planet-test.cc
+++ tests/planet-test.cc
@@ -2,3 +2,12 @@
 // Created by Venya Joshi on 4/20/21.
 //
 
+#include <solarsystem.h>
+#include <catch2/catch.hpp>
+#include <iostream>
+#include "../include/solarsystem.h"
+
+TEST_CASE("Test parsing of json") {
+    solarsystem::SolarSystem solarSystem("planet.json");
+    REQUIRE(solarSystem.GetPlanets().at(0).GetName() == "Sun");
+}
