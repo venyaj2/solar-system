1d7de185009c2ef220309b75ee0914d93fdad719
venyajoshi <venyaj2@illinois.edu>
2021-04-22 12:14:07 -0500

    Created incorrectly initialized solar syste tests

---
+++ tests/solarsystem-test.cc
@@ -0,0 +1,11 @@
+//
+// Created by Venya Joshi on 4/22/21.
+//
+
+#include <catch2/catch.hpp>
+#include <iostream>
+#include <planet.h>
+
+TEST_CASE("Test incorrectly initialized solarsystem") {
+    
+}
