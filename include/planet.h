26894f1915a30571df00f9d87a1d5cd9cbe80311
venyajoshi <venyaj2@illinois.edu>
2021-04-22 12:15:37 -0500

    Added proper constructor

--- include/planet.h
+++ include/planet.h
@@ -1,13 +1,14 @@
 //
 // Created by Venya Joshi on 4/20/21.
 //
-
+#pragma once
 #ifndef SOLAR_SYSTEM_PLANET_H
 #define SOLAR_SYSTEM_PLANET_H
 #include <string>
 #endif //SOLAR_SYSTEM_PLANET_H
 
 namespace solarsystem {
+    
     class Planet {
     public:
 
@@ -30,7 +31,67 @@ namespace solarsystem {
             return distance_from_center_;
         }
         
+        std::string GetName() const {
+            return name_;
+        }
         
+        void SetDegree(float degree) {
+            degree_from_center_ = degree;
+        }
+        
+        /**
+         * Enum for Sun
+         */
+        static const Planet SUN;
+
+        /**
+         * Enum for Mercury
+         */
+        static const Planet MERCURY;
+
+        /**
+         * Enum for Venus
+         */
+        static const Planet VENUS;
+
+        /**
+         * Enum for Earth
+         */
+        static const Planet EARTH;
+
+        /**
+         * Enum for Mars
+         */
+        static const Planet MARS;
+
+        /**
+         * Enum for Jupiter
+         */
+        static const Planet JUPITER;
+
+        /**
+         * Enum for Saturn 
+         */
+        static const Planet SATURN;
+
+        /**
+         * Enum for Uranus
+         */
+        static const Planet URANUS;
+
+        /**
+         * Enum for Neptune
+         */
+        static const Planet NEPTUNE;
+
+        /**
+             * Creates a new planet instance
+             * @param mass of planet
+             * @param radius of planet
+             * @param color of planet
+             */
+        Planet(const std::string name, float radius, const std::string color, float degree_from_center, float distance_from_center, std::string description);
+
     private:
         /**
          * Name of planet
@@ -61,14 +122,7 @@ namespace solarsystem {
         /**
          * The description of the planet
          */
-         float description_;
+         std::string description_;
 
-        /**
-         * Creates a new planet instance
-         * @param mass of planet
-         * @param radius of planet
-         * @param color of planet
-         */
-        Planet(const std::string& name, float radius, const std::string& color, float degree_from_center, float distance_from_center, std::string description);
     };
 }
