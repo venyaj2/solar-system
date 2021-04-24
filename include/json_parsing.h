387b301db0b01bdad5de6a91b70e2292581a9fab
venyajoshi <venyaj2@illinois.edu>
2021-04-23 19:45:57 -0500

    Added sphere class to create planet spheres

--- include/json_parsing.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Venya Joshi on 4/21/21.
-//
-
-#ifndef SOLAR_SYSTEM_JSON_PARSING_H
-#define SOLAR_SYSTEM_JSON_PARSING_H
-
-#endif //SOLAR_SYSTEM_JSON_PARSING_H
---
+++ include/sphere.h
@@ -0,0 +1,8 @@
+//
+// Created by Venya Joshi on 4/21/21.
+//
+
+#ifndef SOLAR_SYSTEM_SPHERE_H
+#define SOLAR_SYSTEM_SPHERE_H
+
+#endif //SOLAR_SYSTEM_SPHERE_H
