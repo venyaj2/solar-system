971252703eebeaf2178c090191acc336d4d5f2d3
venyajoshi <venyaj2@illinois.edu>
2021-04-27 20:31:56 -0500

    Adjusted planet test

--- tests/planet-test.cc
+++ tests/planet-test.cc
@@ -7,10 +7,10 @@
 #include <planet.h>
 
 TEST_CASE("Test proper initialization of planet") {
-    solarsystem::Planet planet("Pluto", 4, "gray", 55, 700, "Dwarf planet, formally removed from planet circulation by NASA");
+    solarsystem::Planet planet("Pluto", 4, "gray", 55, 19, 700, "Dwarf planet, formally removed from planet circulation by NASA",8, 0);
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 55);
-        REQUIRE(planet.GetDistance() == 700);
+        REQUIRE(planet.GetDistanceX() == 700);
         REQUIRE(planet.GetRadius() == 4);
         REQUIRE(planet.GetColor() == std::string("gray"));
     }
@@ -25,7 +25,7 @@ TEST_CASE("Test Sun setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::SUN;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 0);
-        REQUIRE(planet.GetDistance() == 0);
+        REQUIRE(planet.GetDistanceX() == 0);
         REQUIRE(planet.GetRadius() == 30);
         REQUIRE(planet.GetColor() == std::string("yellow"));
     }
@@ -40,7 +40,7 @@ TEST_CASE("Test Mercury setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::MERCURY;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 57);
-        REQUIRE(planet.GetDistance() == 50);
+        REQUIRE(planet.GetDistanceX() == 50);
         REQUIRE(planet.GetRadius() == 4);
         REQUIRE(planet.GetColor() == std::string("gray"));
     }
@@ -55,7 +55,7 @@ TEST_CASE("Test Venus setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::VENUS;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 87);
-        REQUIRE(planet.GetDistance() == 70);
+        REQUIRE(planet.GetDistanceX() == 70);
         REQUIRE(planet.GetRadius() == 6);
         REQUIRE(planet.GetColor() == std::string("brown"));
     }
@@ -70,7 +70,7 @@ TEST_CASE("Test Earth setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::EARTH;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 97);
-        REQUIRE(planet.GetDistance() == 80);
+        REQUIRE(planet.GetDistanceX() == 80);
         REQUIRE(planet.GetRadius() == 10);
         REQUIRE(planet.GetColor() == std::string("green"));
     }
@@ -85,7 +85,7 @@ TEST_CASE("Test Mars setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::MARS;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 137);
-        REQUIRE(planet.GetDistance() == 98);
+        REQUIRE(planet.GetDistanceX() == 98);
         REQUIRE(planet.GetRadius() == 8);
         REQUIRE(planet.GetColor() == std::string("red"));
     }
@@ -100,7 +100,7 @@ TEST_CASE("Test Jupiter setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::JUPITER;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 200);
-        REQUIRE(planet.GetDistance() == 180);
+        REQUIRE(planet.GetDistanceX() == 180);
         REQUIRE(planet.GetRadius() == 17);
         REQUIRE(planet.GetColor() == std::string("orange"));
     }
@@ -115,7 +115,7 @@ TEST_CASE("Test Saturn setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::SATURN;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 193);
-        REQUIRE(planet.GetDistance() == 280);
+        REQUIRE(planet.GetDistanceX() == 280);
         REQUIRE(planet.GetRadius() == 14);
         REQUIRE(planet.GetColor() == std::string("yellow"));
     }
@@ -130,7 +130,7 @@ TEST_CASE("Test Uranus setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::URANUS;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 280);
-        REQUIRE(planet.GetDistance() == 300);
+        REQUIRE(planet.GetDistanceX() == 300);
         REQUIRE(planet.GetRadius() == 6);
         REQUIRE(planet.GetColor() == std::string("teal"));
     }
@@ -145,7 +145,7 @@ TEST_CASE("Test Neptune setters and getters") {
     solarsystem::Planet planet = solarsystem::Planet::NEPTUNE;
     SECTION("Test initialized Planet getters") {
         REQUIRE(planet.GetAngle() == 300);
-        REQUIRE(planet.GetDistance() == 320);
+        REQUIRE(planet.GetDistanceX() == 320);
         REQUIRE(planet.GetRadius() == 6);
         REQUIRE(planet.GetColor() == std::string("blue"));
     }
