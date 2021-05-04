ddf6f40074aa9cfaafffaac0579a43c96b6129a7
venyajoshi <venyaj2@illinois.edu>
2021-05-04 10:33:33 -0500

    Implemented mouse down for description

--- include/solarsystem_simulation.h
+++ include/solarsystem_simulation.h
@@ -26,9 +26,8 @@ namespace solarsystem {
 
         void update() override;
 
-        /*void mouseDown(ci::app::MouseEvent event) override;
-        void mouseDrag(ci::app::MouseEvent event) override;
-        void keyDown(ci::app::KeyEvent event) override;*/
+        void mouseDown(ci::app::MouseEvent event) override;
+        void keyDown(ci::app::KeyEvent event) override;
 
     private:
         solarsystem::SolarSystem solarsystem = SolarSystem(900, 900);
