7ad81b5b42be2dd3e6a16ac55a19a32e5a0350ca
venyajoshi <venyaj2@illinois.edu>
2021-05-04 19:14:03 -0500

    Revert "Removed description from planet header"
    This reverts commit 7cc27490

--- src/planet.cc
+++ src/planet.cc
@@ -13,10 +13,10 @@ namespace solarsystem {
     Planet::Planet() {}
     
     Planet::Planet(const std::string name, float radius, const std::string color, float degree_from_center,
-                   float distance_from_center_x, float distance_from_center_y, float velocity,
+                   float distance_from_center_x, float distance_from_center_y, std::string description, float velocity,
                    size_t number_of_moons, const std::string file) {
         
-        if (radius < 0 || name == "" || color == "" || distance_from_center_x < 0 || distance_from_center_y < 0) {
+        if (radius < 0 || name == "" || color == "" || distance_from_center_x < 0 || distance_from_center_y < 0 || description == "") {
             throw std::invalid_argument("Invalid Planet parameter passed");
         }
         
@@ -27,6 +27,7 @@ namespace solarsystem {
         degree_from_center_ = degree_from_center;
         distance_from_center_y_ = distance_from_center_y;
         distance_from_center_x_ = distance_from_center_x;
+        description_ = description;
         velocity_ = velocity;
         file_ = file;
         visible_description_ = false;
@@ -39,21 +40,21 @@ namespace solarsystem {
     }
     
     //Initializes planets in solarsystem
-    const Planet Planet::SUN = Planet ("Sun", 40, "gold", 0, 0, 0,0,0, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/sun");
+    const Planet Planet::SUN = Planet ("Sun", 40, "gold", 0, 0, 0,"Moons: None \nDistance from Sun: 0 million mi\nRadius: 1,516 mi\nDiameter: 4,879 km\nOrbital period: 88 days \nMass: 3.285 × 10^23 kg\nSurface Temperature: -173 to 427°C", 0,0, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/sun");
 
-    const Planet Planet::MERCURY = Planet("Mercury", 4, "gray", 55, 45,55, 3, 0, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/mercury");
+    const Planet Planet::MERCURY = Planet("Mercury", 4, "gray", 55, 45,55, "Radius: 432,690 mi\nDiameter: 1.3927 million km\nMass: 31.989 × 10^30 kg\nSurface Temperature: 5,505°C\nNumber of Moons: None\nFirst Record: 14th century BCE by Assyrian astronomers", 3, 0, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/mercury");
     
-    const Planet Planet::VENUS = Planet("Venus", 6, "brown", 75, 84, 65, 2, 0, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/venus");
+    const Planet Planet::VENUS = Planet("Venus", 6, "brown", 75, 84, 65, "Venus", 2, 0, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/venus");
 
-    const Planet Planet::EARTH = Planet("Earth", 10, "green", 100, 98, 100, 1.3, 1, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/earth");
+    const Planet Planet::EARTH = Planet("Earth", 10, "green", 100, 98, 100, "Earth", 1.3, 1, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/earth");
 
-    const Planet Planet::MARS = Planet("Mars", 8, "red", 127, 160, 150, 1, 1, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/mars");
+    const Planet Planet::MARS = Planet("Mars", 8, "red", 127, 160, 150, "Mars", 1, 1, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/mars");
 
-    const Planet Planet::JUPITER = Planet("Jupiter", 17, "orange", 200, 240, 278, .8, 11, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/jupiter");
+    const Planet Planet::JUPITER = Planet("Jupiter", 17, "orange", 200, 240, 278, "Jupiter", .8, 11, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/jupiter");
 
-    const Planet Planet::SATURN = Planet("Saturn", 14, "yellow", 193, 340, 310, .7, 5, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/saturn");
+    const Planet Planet::SATURN = Planet("Saturn", 14, "yellow", 193, 340, 310, "Saturn", .7, 5, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/saturn");
 
-    const Planet Planet::URANUS = Planet("Uranus", 11, "teal", 280, 390, 340, .3, 2, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/uranus");
+    const Planet Planet::URANUS = Planet("Uranus", 11, "teal", 280, 390, 340, "Uranus", .3, 2, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/uranus");
 
-    const Planet Planet::NEPTUNE = Planet("Neptune", 10, "blue", 300, 430, 400, .2, 3, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/neptune");
+    const Planet Planet::NEPTUNE = Planet("Neptune", 10, "blue", 300, 430, 400, "Neptune", .2, 3, "/Users/venyajoshi/Downloads/Cinder/my-projects/final-project-venyaj2/assets/neptune");
 } //namespace solarsystem
